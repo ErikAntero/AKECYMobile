baff981e66215f89104aa4fad1f84af50978f5a7
arthuralvesz2 <arthur_alves.2007@hotmail.com>
2024-06-14 23:27:07 -0300

    Tela de cadastro2.xml 14/06/2024

--- app/src/main/java/com/fieb/akecy/MainActivity.java
+++ app/src/main/java/com/fieb/akecy/MainActivity.java
@@ -1,8 +1,9 @@
 package com.fieb.akecy;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 }
